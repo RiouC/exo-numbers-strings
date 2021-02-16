2cbb6940a8d3d54137c9b98e3c0cc9c1c1c85505
RiouC <riou.christophe@protonmail.com>
2021-02-16 14:41:26 +0100

    convert : add convert.js

---
+++ convert.js
@@ -0,0 +1,12 @@
+let numbers = [10,
+	       15,
+	       16,
+	       5005,
+	       52390903];
+
+for (let number of numbers) {
+    console.log(`base 2 : ${number.toString(2)}`);
+    console.log(`base 8 : ${number.toString(8)}`);
+    console.log(`base 16: ${number.toString(16)}\n`);
+}
+
